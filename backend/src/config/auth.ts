29225f95b50f11d597c2f90d5e4c6f9e0d74b391
Danish K <125777715+dani-s-h007@users.noreply.github.com>
2026-07-01 16:28:37 +0530

    Change JWT and refresh secret values
    Updated JWT secret and refresh secret values to new hardcoded values.

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
-  secret: process.env.JWT_SECRET || "mysecret",
+  secret: "3a7b9c1d5e2f4a0b8c6d7e9f1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b",
   expiresIn: "15m",
-  refreshSecret: process.env.JWT_REFRESH_SECRET || "myanothersecret",
+  refreshSecret: "8f9e2b4c7a1d5e0f3b6c8a9d0e2f4a5b6c7d8e9f0a1b2c3d4e5f6a7b8c9d0e1f",
   refreshExpiresIn: "7d"
 };
